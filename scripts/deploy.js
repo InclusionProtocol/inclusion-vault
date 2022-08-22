c3852db15b20aa32b4e75cdcdafd220e5fde4a2b
Stickford <stickfordjoseph@gmail.com>
2022-08-22 11:20:58 +0800

    feat: add fe log

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,7 +8,7 @@ const hre = require("hardhat");
 const { ethers } = require("hardhat");
 const dotenv = require('dotenv');
 
-
+const result = {}
 async function main() {
   dotenv.config();
 
@@ -19,31 +19,39 @@ async function main() {
   const usdt = await USDT.deploy(1_000_000 * deci);
   await usdt.deployed();
   console.log("USDT deployed to:", usdt.address);
+  result.USDT = usdt.address
 
   const SDGI = await ethers.getContractFactory("SDGI");
   const sdgi = await SDGI.deploy(1_000_000 * deci);
   await sdgi.deployed();
   console.log("SDGI deployed to:", sdgi.address);
+  result.SDGI = sdgi.address
 
   const SDG2USD = await ethers.getContractFactory("SDG2USD");
   const sdg2usd = await SDG2USD.deploy();
   await sdg2usd.deployed();
   console.log("SDG2USD ratio oracle deployed to:", sdg2usd.address);
+  result.SDG2USD = sdg2usd.address
 
   const Exchange = await ethers.getContractFactory("Exchange");
   const exchange = await Exchange.deploy(usdt.address, sdgi.address, admin, sdg2usd.address);
   await exchange.deployed();
   console.log("Exchange deployed to:", exchange.address);
+  result.Exchange = exchange.address
 
   const Vault = await ethers.getContractFactory("Vault");
   const vault = await Vault.deploy("SDGI", sdgi.address, admin);
   await vault.deployed();
   console.log("Vault deployed to:", vault.address);
+  result.Vault = vault.address
 
   const SwapAndDeposit = await ethers.getContractFactory("SwapAndDeposit");
   const swapanddeposit = await SwapAndDeposit.deploy(usdt.address, sdgi.address, exchange.address, vault.address);
   await swapanddeposit.deployed();
   console.log("SwapAndDeposit deployed to:", swapanddeposit.address);
+  result.SwapAndDeposit = swapanddeposit.address
+
+  console.log('\n\nresult => \n', JSON.stringify(result, null, 2))
 }
 
 // We recommend this pattern to be able to use async/await everywhere
